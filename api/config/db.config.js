e648e7eaf82b97535fe6edfc6a50bafb079231ba
Anna Soriano <annasorianotoledo@gmail.com>
2024-09-23 16:40:10 +0200

    .

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/spotify-playlist';
+const MONGODB_URI = process.env.MONGODB_URI // || 'mongodb://localhost/spotify-playlist';
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
